92dba8fabe4c90f21fedaa3074ef87e3c217cac0
Jakub Vaněk <linuxtardis@gmail.com>
2025-11-03 20:26:36 +0100

    Add attestation certificates in file form

---
+++ attestation_cert/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -xe
+
+openssl ecparam -out attestation_key.pem -name prime256v1 -genkey
+openssl pkcs8 -topk8 -inform PEM -outform PEM -in attestation_key.pem -out attestation_key_pkcs8.pem -nocrypt
+openssl req -new -sha256 -key attestation_key.pem -out attestation_cert.csr -config attestation_cert.conf
+openssl x509 -req -sha256 -in attestation_cert.csr -signkey attestation_key.pem -out attestation_cert.pem -days 3650 -extfile attestation_cert.conf -extensions v3_req
+openssl x509 -in attestation_cert.pem -inform PEM -out attestation_cert.crt -outform DER
+
+base64 attestation_cert.crt | sed 's|+|-|g; s|/|_|g; s|=||g' >attestation_cert.crt.base64
